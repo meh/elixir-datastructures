a277d1d37ad9776cbc85a94e9570a28a21e761fb
meh <meh@schizofreni.co>
2013-06-28 12:14:21 +0200

    test: fix up stack tests

---
+++ test/stack/simple_test.exs
@@ -0,0 +1,76 @@
+Code.require_file "../test_helper.exs", __DIR__
+
+defmodule StackSimpleTest do
+  use ExUnit.Case, async: true
+  alias Data.Stack
+  alias Data.Stack.Simple
+
+  test :push do
+    assert Simple.new |> Stack.push(42) |> Data.to_list == [42]
+    assert Simple.new |> Stack.push(42) |> Stack.push(23) |> Data.to_list == [23, 42]
+  end
+
+  test :pop do
+    assert { 23, _ } = Simple.new |> Stack.push(23) |> Stack.pop
+    assert { 42, _ } = Simple.new |> Stack.pop(42)
+  end
+
+  test :pop! do
+    assert { 23, _ } = Simple.new |> Stack.push(23) |> Stack.pop!
+
+    assert_raise Data.Empty, fn ->
+      Simple.new |> Stack.pop!
+    end
+  end
+
+  test :peek do
+    assert Simple.new |> Stack.push(23) |> Data.peek == 23
+    assert Simple.new |> Data.peek(42) == 42
+  end
+
+  test :peek! do
+    assert Simple.new |> Stack.push(23) |> Data.peek! == 23
+
+    assert_raise Data.Empty, fn ->
+      Simple.new |> Data.peek!
+    end
+  end
+
+  test :first do
+    assert Simple.new |> Data.first == nil
+    assert Simple.new |> Stack.push(42) |> Data.first == 42
+  end
+
+  test :next do
+    assert Simple.new |> Data.next == nil
+    assert Simple.new |> Stack.push(42) |> Data.next == nil
+    assert Simple.new |> Stack.push(42) |> Stack.push(23) |> Data.next |> Data.to_list == [42]
+  end
+
+  test :reverse do
+    assert Simple.new |> Data.reverse |> Data.to_list == []
+    assert Simple.new |> Stack.push(23) |> Stack.push(42) |> Data.reverse |> Data.to_list == [23, 42]
+  end
+
+  test :empty? do
+    assert Simple.new |> Data.empty?
+    refute Simple.new |> Stack.push(23) |> Data.empty?
+  end
+
+  test :clear do
+    assert Simple.new |> Stack.push(23) |> Data.clear |> Data.empty?
+  end
+
+  test :to_list do
+    assert Simple.new |> Stack.push(23) |> Stack.push(42) |> Data.to_list == [42, 23]
+  end
+
+  test :contains? do
+    refute Simple.new |> Data.contains?(23)
+    assert Simple.new |> Stack.push(42) |> Data.contains?(42)
+  end
+
+  test Enumerable do
+    assert Simple.new |> Stack.push(23) |> Stack.push(42) |> Enum.to_list == [42, 23]
+  end
+end
