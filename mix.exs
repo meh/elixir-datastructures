31a28f4b6f789a77b8f4fa083adacf8911c23b6b
meh <meh@schizofreni.co>
2016-05-21 16:47:32 +0200

    mix: bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Datastructures.Mixfile do
 
   def project do
     [ app: :datastructures,
-      version: "0.2.0",
+      version: "0.2.2",
       deps: deps,
       package: package,
       description: "Elixir protocols and implementations for various data structures." ]
